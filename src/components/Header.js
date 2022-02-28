1a8d92f8700f79f2201d9131b44d0edf9b0cd2f3
mukhiil <bmukhiil@gmail.com>
2022-02-27 22:21:49 -0800

    homepage changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,14 @@ import './Header.css';
 function Header() {
   return(
     <div class="nav-container">
-      <ul>
+      <ul class="nav-list-items">
         <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
         <li class="nav-item"><a class="nav-link" href="/">Portfolio</a></li>
         <li class="nav-item"><a class="nav-link" href="/">Jobs</a></li>
         <li class="nav-item"><a class="nav-link" href="/">About</a></li>
         <li class="nav-item"><a class="nav-link" href="/">Contact</a></li>
       </ul>
+      <div class="under-nav-div"></div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,22 @@ function Home() {
     <div className="App">
       
       <header className="App-header">
-        <Header/>
-      
+        <div className="top-margin"></div>
+        <Header className="header"/>
+        < div class="middle-text">
+            <div class="middle-top-div"></div>
+            <h1>Hire Trained Candidates Quickly From Us!</h1>
+            <h4>Connect with us to explore our candidate pool who are trained in Tech Companies' Emerging Tech Skill<br></br>requirements like ML, AI, AR based hands on projects.</h4>
+          </div>
+        <div class="middle-image">
+
+        </div>
       </header>
+
+      <div className="Looking-For-Engineers">
+      <div className="Looking-For-Engineers-Top"></div>
+          <h1>Looking for First-Class ML Engineers?</h1>
+      </div>
     </div>
   );
 }
